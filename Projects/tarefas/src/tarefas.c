a6758cd7c72413c6853c7ae36b32f274d84019fb
VictorSLR <victorslr@users.noreply.github.com>
2021-03-18 23:19:18 -0300

    Lab04

--- Projects/tarefas/src/tarefas.c
+++ Projects/tarefas/src/tarefas.c
@@ -2,39 +2,45 @@
 #include "driverleds.h" // device drivers
 #include "cmsis_os2.h" // CMSIS-RTOS
 
-osThreadId_t thread1_id, thread2_id;
+osThreadId_t thread1_id, thread2_id, thread3_id, thread4_id;
+
+typedef struct leds_arg {
+   int led;
+   int tempo;
+} leds_arg;
 
 void thread1(void *arg){
+  leds_arg* thr_led = (leds_arg*) arg;
   uint8_t state = 0;
   
   while(1){
-    state ^= LED1;
-    LEDWrite(LED1, state);
-    osDelay(100);
+    state ^= thr_led->led;
+    LEDWrite(thr_led->led, state);
+    osDelay(thr_led->tempo);
   } // while
 } // thread1
 
-void thread2(void *arg){
-  uint8_t state = 0;
-  uint32_t tick;
-  
-  while(1){
-    tick = osKernelGetTickCount();
-    
-    state ^= LED2;
-    LEDWrite(LED2, state);
-    
-    osDelayUntil(tick + 100);
-  } // while
-} // thread2
 
 void main(void){
-  LEDInit(LED2 | LED1);
+  LEDInit(LED2 | LED1 | LED3 | LED4);
 
   osKernelInitialize();
+  
+  leds_arg led1, led2, led3, led4;
+  
+  led1.led = LED1;
+  led1.tempo = 200;
+  led2.led = LED2;
+  led2.tempo = 300;
+  led3.led = LED3;
+  led3.tempo = 500;
+  led4.led = LED4;
+  led4.tempo = 700;
 
-  thread1_id = osThreadNew(thread1, NULL, NULL);
-  thread2_id = osThreadNew(thread2, NULL, NULL);
+  thread1_id = osThreadNew(thread1, &led1, NULL);
+  thread2_id = osThreadNew(thread1, &led2, NULL);
+  thread3_id = osThreadNew(thread1, &led3, NULL);
+  thread4_id = osThreadNew(thread1, &led4, NULL);
 
   if(osKernelGetState() == osKernelReady)
     osKernelStart();
